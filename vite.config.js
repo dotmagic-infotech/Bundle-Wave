bb7daba321b5065e209b61c324d0b0e6414e6b36
dotmagic-infotech <info@dotmagicinfotech.com>
2025-08-14 12:37:50 +0530

    Update code

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,41 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   server: {
+//     host: "0.0.0.0",
+//     port: 8989,
+//     proxy: {
+//       '/api': 'http://test-app.dotmagicinfotech.in',
+//     },
+//     allowedHosts: true,
+//   },
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 8989,
-    proxy: {
-      '/api': 'http://test-app.dotmagicinfotech.in',
-    },
-    allowedHosts: true,
-  },
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve'
+
+  return {
+    plugins: [react()],
+    server: isDev
+      ? {
+        host: "0.0.0.0",
+        port: 8989,
+        proxy: {
+          '/api': 'http://test-app.dotmagicinfotech.in',
+        },
+        allowedHosts: true,
+      }
+      : undefined,
+    build: {
+      outDir: 'dist', // Vercel will serve this in production
+    }
+  }
 })
