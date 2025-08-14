0f683af1b343ceaeef0f9d5e48d63b93dbdba9c4
dotmagic-infotech <info@dotmagicinfotech.com>
2025-08-14 14:49:28 +0530

    upadet code

--- vite.config.js
+++ vite.config.js
@@ -19,18 +19,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '',
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     host: "0.0.0.0",
     port: 8989,
     headers: {
       'Content-Security-Policy': 'frame-ancestors https://*.myshopify.com https://admin.shopify.com',
       'X-Frame-Options': 'ALLOWALL',
-    }
+    },
+    allowedHosts: true,
   },
-  build: {
-    outDir: 'dist'
-  },
-  plugins: [react()]
 })
 
