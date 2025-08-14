7125d69a30c8d2d6219226bf066d9ade4698e377
dotmagic-infotech <info@dotmagicinfotech.com>
2025-08-14 14:21:05 +0530

    update changes

--- vite.config.js
+++ vite.config.js
@@ -18,15 +18,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 8989,
-    headers: {
-      'Content-Security-Policy': 'frame-ancestors https://*.myshopify.com https://admin.shopify.com',
-      'X-Frame-Options': 'ALLOWALL',
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve'
+
+  return {
+    base: './', // important for Vercel deploy
+    plugins: [react()],
+    server: isDev
+      ? {
+        host: "0.0.0.0",
+        port: 8989,
+        headers: {
+          'Content-Security-Policy': "frame-ancestors https://*.myshopify.com https://admin.shopify.com",
+          'X-Frame-Options': 'ALLOWALL',
+        },
+        proxy: {
+          '/api': 'http://test-app.dotmagicinfotech.in'
+        }
+      }
+      : undefined,
+    build: {
+      outDir: 'dist'
     }
-  },
-  plugins: [react()]
+  }
 })
 
