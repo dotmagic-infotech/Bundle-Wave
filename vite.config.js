c9fb78ab150d1206773718ef40539b8b5c8027ab
dotmagic-infotech <info@dotmagicinfotech.com>
2025-08-21 14:57:36 +0530

    Update code

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   server: {
-//     host: "0.0.0.0",
-//     port: 8989,
-//     proxy: {
-//       '/api': 'http://test-app.dotmagicinfotech.in',
-//     },
-//     allowedHosts: true,
-//   },
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
